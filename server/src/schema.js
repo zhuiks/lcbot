d3b08e5a84aced65868b4def47f7856481e91861
zhuiks <zhuke@ukr.net>
2020-01-19 15:59:06 +0200

    first schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,6 +2,26 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   # Your schema will go here
+  type Query {
+    songs: [Song]!
+    song(id: ID!): Song
+  }
+
+  type Song {
+      id: ID!
+      title: String
+      text: [Slide!]!
+      links: [String]
+  }
+
+  type Slide {
+      id: ID!
+      lines: [String!]!
+  }
+
+  type Mutation {
+      saveSong(title: String, text: [String], link: String): String
+  }
 `;
 
 module.exports = typeDefs;
