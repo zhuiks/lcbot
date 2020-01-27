0774f04117e954271f6f961200e0a4075f572244
zhuiks <zhuke@ukr.net>
2020-01-27 14:52:56 +0200

    save song

--- client/src/components/song-form.tsx
+++ client/src/components/song-form.tsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 interface SaveFormProps {
   saveSong: (a: { variables: any }) => void;
@@ -11,40 +13,79 @@ interface SaveFormState {
 }
 
 export default class SaveForm extends Component<SaveFormProps, SaveFormState> {
-  state = { 
+  state = {
     title: '',
     text: [''],
     links: ['']
-   };
+  };
 
-  // onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const title = (event.target as HTMLInputElement).value;
-  //   this.setState(s => ({ title: title }));
-  // };
+  onChange = (el: HTMLInputElement) => {
+    console.log(`${el.id} = "${el.value}"`);
+    let newState: any = {};
+    switch(el.id) {
+      case "newSongForm.Title":
+        newState = {title: el.value.trim()};
+        break;
+      case "newSongForm.Text":
+        newState = el.value.split(/\r\n|[\n\v\f\r\x85\u2028\u2029]/);
+        if(this.state.title.length === 0 && newState.length > 1) {
+          newState = {
+            text: newState,
+            title: newState[0].trim() //todo: update html input
+          }
+        } else {
+          newState = {
+            text: newState
+          }
+        }
+        break;
+      case "newSongForm.Link":
+        newState = {
+          links: [el.value]
+        }
+        break;
+      }
+    console.log(newState);  
+    this.setState(newState);
+  };
 
-  // onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   this.props.saveSong({ variables: { 
-  //     title: this.state.title,
-  //     text: this.state.text,
-  //     links: this.state.links
-  //    } });
-  // };
+  onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.saveSong({ variables: { 
+      title: this.state.title,
+      text: this.state.text,
+      links: this.state.links
+     } });
+  };
 
   render() {
     return (
+      <Fragment>
         <h1>New Song</h1>
-        // <StyledForm onSubmit={(e) => this.onSubmit(e)}>
-        //   <StyledInput
-        //     required
-        //     type="text"
-        //     name="title"
-        //     placeholder="Title"
-        //     data-testid="title-input"
-        //     onChange={(e) => this.onChange(e)}
-        //   />
-        //   <Button type="submit">Save</Button>
-        // </StyledForm>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group controlId="newSongForm.Title">
+            <Form.Control
+              placeholder="Song title (not required)"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {this.onChange(e.target)}}
+            />
+          </Form.Group>
+          <Form.Group controlId="newSongForm.Text">
+            <Form.Label>Song Lyrics:</Form.Label>
+            <Form.Control 
+              as="textarea"
+              rows="10"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {this.onChange(e.target)}}
+            />
+          </Form.Group>
+          <Form.Group controlId="newSongForm.Link">
+            <Form.Label>Video Link:</Form.Label>
+            <Form.Control
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {this.onChange(e.target)}}
+            />
+          </Form.Group>
+          <Button type="submit">Save</Button>
+        </Form>
+      </Fragment>
     );
   }
 }
