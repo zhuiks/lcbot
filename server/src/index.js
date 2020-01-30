9212f015cb29666ca77e99fc43227ee9740f8a0d
zhuiks <zhuke@ukr.net>
2020-01-30 13:09:45 +0200

    deployment refractor

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,4 @@
-require('dotenv').config()
-
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-lambda');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const Songs = require('./datasources/songs-dynamodb');
