8def4b5f31dcf7a10a4eb3c525f9696c819828a1
zhuiks <zhuke@ukr.net>
2020-01-30 14:10:47 +0200

    fix import and refactor

--- server/src/handler.js
+++ server/src/handler.js
@@ -1,1 +1,25 @@
-module.exports.query = require('./index').handler
+const { ApolloServer } = require('apollo-server-lambda');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const Songs = require('./datasources/songs-dynamodb');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  tracing: true,
+  dataSources: () => {
+    return {
+     songs: new Songs()
+  }}
+});
+
+module.exports.query = (event, context, callback) => {
+  const handler = server.createHandler();
+
+  // tell AWS lambda we do not want to wait for NodeJS event loop
+  // to be empty in order to send the response
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  // process the request
+  return handler(event, context, callback);
+};
