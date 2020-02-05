0e27a7d5c218aaece1654e4adcbb2e4b280287a8
zhuiks <zhuke@ukr.net>
2020-02-05 11:51:59 +0200

    gatsby react-bootstrap starter

--- webstatic/src/pages/index.js
+++ webstatic/src/pages/index.js
@@ -1,20 +1,69 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Row, Col, Container, ListGroup } from "react-bootstrap"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  <Layout pageInfo={{ pageName: "index" }}>
+    <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
+    <Container className="text-center">
+      <Row>
+        <Col>
+          <p>
+            This is a Gatsby Starter that I frequently use to get jump started
+            on quick website builds. It includes the following packages:
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center my-3">
+        <Col md="6">
+          <ListGroup>
+            <ListGroup.Item
+              action
+              href="https://getbootstrap.com"
+              target="_blank"
+            >
+              Bootstrap
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://react-bootstrap.github.io/"
+              target="_blank"
+            >
+              react-bootstrap
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://react-icons.netlify.com"
+              target="_blank"
+            >
+              react-icons
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://www.gatsbyjs.org/packages/gatsby-plugin-sass/"
+              target="_blank"
+            >
+              gatsby-plugin-sass
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>
+            This starter also includes a navbar that sticks to the top of the
+            screen when the user scrolls past it, and a footer that stays at the
+            bottom of the screen.
+          </p>
+          <p>
+            For more documentation on these packages and how they work, please
+            refer to the pages linked in the list above.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
