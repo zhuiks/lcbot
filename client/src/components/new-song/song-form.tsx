ccacba5e66d442096f80432de00f898fbb9e2c00
zhuiks <zhuke@ukr.net>
2020-02-10 15:00:02 +0200

    useState hook instead of class

--- client/src/components/new-song/song-form.tsx
+++ client/src/components/new-song/song-form.tsx
@@ -1,87 +1,84 @@
-import React, { Fragment, Component } from 'react';
+import React, { useState, Component } from 'react';
 import StepWizard from 'react-step-wizard';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Step from './step';
+import * as SavingTypes from '../../pages/__generated__/saveSong';
+import { MutationFunctionOptions } from '@apollo/react-common';
 
 interface SaveFormProps {
-  saveSong: (a: { variables: any }) => void;
+  saveSong: (options: MutationFunctionOptions<SavingTypes.saveSong, SavingTypes.saveSongVariables>) => any;
 }
 
-interface SaveFormState {
-  title: string;
-  text: string[];
-  links: string[];
+const SaveForm: React.FC<SaveFormProps> = ({saveSong}) => {
+
+  const [songText, setText] = useState('');
+  const [songTitle, setTitle] = useState('');
+  const [songLinks, setLinks] = useState(['']);
+
+  // onChange = (el: HTMLInputElement) => {
+  //   console.log(`${el.id} = "${el.value}"`);
+  //   let newState: any = {};
+  //   switch (el.id) {
+  //     case "song-title":
+  //       newState = { title: el.value.trim() };
+  //       break;
+  //     case "song-text":
+  //       newState = el.value.split(/\r\n|[\n\v\f\r\x85\u2028\u2029]/);
+  //       if (this.state.title.length === 0 && newState.length > 1) {
+  //         newState = {
+  //           text: newState,
+  //           title: newState[0].trim() //todo: update html input
+  //         }
+  //       } else {
+  //         newState = {
+  //           text: newState
+  //         }
+  //       }
+  //       break;
+  //     case "song-link":
+  //       newState = {
+  //         links: [el.value]
+  //       }
+  //       break;
+  //   }
+  //   console.log(newState);
+  //   this.setState(newState);
+  // };
+
+  const onStepChange = (stats: any) => {
+    console.log(stats);
 }
-
-export default class SaveForm extends Component<SaveFormProps, SaveFormState> {
-  state = {
-    title: '',
-    text: [''],
-    links: ['']
-  };
-
-  onChange = (el: HTMLInputElement) => {
-    console.log(`${el.id} = "${el.value}"`);
-    let newState: any = {};
-    switch (el.id) {
-      case "song-title":
-        newState = { title: el.value.trim() };
-        break;
-      case "song-text":
-        newState = el.value.split(/\r\n|[\n\v\f\r\x85\u2028\u2029]/);
-        if (this.state.title.length === 0 && newState.length > 1) {
-          newState = {
-            text: newState,
-            title: newState[0].trim() //todo: update html input
-          }
-        } else {
-          newState = {
-            text: newState
-          }
-        }
-        break;
-      case "song-link":
-        newState = {
-          links: [el.value]
-        }
-        break;
-    }
-    console.log(newState);
-    this.setState(newState);
-  };
-
-  onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    this.props.saveSong({
+    saveSong({
       variables: {
-        title: this.state.title,
-        text: this.state.text,
-        links: this.state.links
+        title: songTitle,
+        text: [],
+        links: songLinks
       }
     });
   };
 
-  render() {
-    return (
-      <Row className="justify-content-center">
-        <Col sm={6}>
-        <Form onSubmit={this.onSubmit}>
-          <StepWizard>
+  return (
+    <Row className="justify-content-center">
+      <Col sm={6}>
+        <Form onSubmit={onSubmit}>
+          <StepWizard onStepChange={onStepChange}>
             <Step title="Song Lyrics">
-                <Form.Control
-                  id="song-text"
-                  as="textarea"
-                  rows="10"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { this.onChange(e.target) }}
-                />
+              <Form.Control
+                id="song-text"
+                as="textarea"
+                rows="10"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setText(e.target.value) }}
+              />
             </Step>
             <Step title="Song Data">
               <Form.Group controlId="song-title">
                 <Form.Control
                   placeholder="Song title"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { this.onChange(e.target) }}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setTitle(e.target.value) }}
                 />
               </Form.Group>
             </Step>
@@ -89,14 +86,15 @@ export default class SaveForm extends Component<SaveFormProps, SaveFormState> {
               <Form.Group controlId="song-link">
                 <Form.Label>Video Link:</Form.Label>
                 <Form.Control
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { this.onChange(e.target) }}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setLinks([e.target.value]) }}
                 />
               </Form.Group>
             </Step>
           </StepWizard>
         </Form>
-        </Col>
-      </Row>
-    );
-  }
-}
+      </Col>
+    </Row>
+  );
+};
+
+export default SaveForm;
