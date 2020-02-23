a03d038ac7badf2f9fba8a6eafd9b7786ab757b7
zhuiks <zhuke@ukr.net>
2020-02-23 14:17:57 +0200

    attempt to use bit song-text

--- editor/src/components/text-form/orderer.tsx
+++ editor/src/components/text-form/orderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { SlideInput } from '../../__generated__/globalTypes';
+import SongText from '@bit/zhuiks.lcbot.song-text';
 
 interface OrdererProps {
     slides: SlideInput[] | undefined;
@@ -8,20 +9,21 @@ interface OrdererProps {
 
 const Orderer: React.FC<OrdererProps> = ({ slides }) => {
 
-    return (
-        <>
-            {slides && slides.map((slide: SlideInput, n: number) => (
-                <Card key={n} bg={slide.type==="VERSE" ? "primary" : "secondary"} text="white" className="mb-3">
-                    <Card.Header>{slide.name}</Card.Header>
-                    <Card.Body>
-                        {slide.lines && slide.lines.map((line: string, i: number) => (
-                            <p key={i} className="mb-0">{line}</p>
-                        ))}
-                    </Card.Body>
-                </Card>
-            ))}
-        </>
-    );
+    return <SongText slides={slides} />
+    // return (
+    //     <>
+    //         {slides && slides.map((slide: SlideInput, n: number) => (
+    //             <Card key={n} bg={slide.type==="VERSE" ? "primary" : "secondary"} text="white" className="mb-3">
+    //                 <Card.Header>{slide.name}</Card.Header>
+    //                 <Card.Body>
+    //                     {slide.lines && slide.lines.map((line: string, i: number) => (
+    //                         <p key={i} className="mb-0">{line}</p>
+    //                     ))}
+    //                 </Card.Body>
+    //             </Card>
+    //         ))}
+    //     </>
+    // );
 }
 
 export default Orderer;
