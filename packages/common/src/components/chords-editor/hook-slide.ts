f36170a99096e86f1662488fbff6d3a86228bd7e
zhuiks <zhuke@ukr.net>
2020-03-20 12:56:55 +0200

    refactor

--- packages/common/src/components/chords-editor/hook-slide.ts
+++ packages/common/src/components/chords-editor/hook-slide.ts
@@ -1,7 +1,9 @@
 import { useReducer } from "react";
-import { EditorState } from "draft-js";
+import { EditorState, SelectionState, Modifier, ContentState } from "draft-js";
 import ChordSlide from "../../chords/chord-slide";
-import { initChords, applyChord } from "./apply-chords";
+
+const CHORD_TYPE = 'CHORD';
+const CHORD_MUTABILITY = 'IMMUTABLE';
 
 interface SlideEditorState {
     chordSlide: ChordSlide;
@@ -74,4 +76,52 @@ export const useSlide = (initialSlide: ChordSlide) => {
         editorState: state.editorState,
         dispatch
     }
+}
+
+const getCharacterLength = (str: string) => {
+    // The string iterator that is used here iterates over characters,
+    //  not mere code units
+    return [...str].length;
+}
+
+
+const applyChord = (
+    chordData: IChord,
+    state: ContentState,
+    line: number,
+    pos: number
+) => {
+    // const blockMap = state.getBlockMap();
+    // const entityMap = state.getEntityMap();
+
+    const theBlock = state.getBlockMap().toIndexedSeq().get(line);
+    const selection = new SelectionState({
+        'anchorKey': theBlock.getKey(),
+        'anchorOffset': pos,
+        'focusKey': theBlock.getKey(),
+        'focusOffset': pos + getCharacterLength(chordData.text),
+        'isBackward': false,
+        'hasFocus': true,
+    });
+
+    const newState = state.createEntity(CHORD_TYPE, CHORD_MUTABILITY, {chord: chordData});
+    const entityKey = newState.getLastCreatedEntityKey();
+
+    return Modifier.applyEntity(newState, selection, entityKey);
+}
+
+const initChords = (
+    slide: ChordSlide,
+    initState: ContentState | null = null
+) => {
+    let state = initState || ContentState.createFromText(slide.lines?.join('\n') || '');
+    for (let l=0; l<slide.chords.size; l++ ) {
+        let offset = 0;
+        for (let k=0; k<slide.chords.get(l).size; k++ ) {
+            const chord = slide.getChord(l, k);
+            state = applyChord(chord, state, l, offset);
+            offset += getCharacterLength(chord.text);
+        }
+    }
+    return state;
 }
