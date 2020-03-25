6fa3f9a64ed3f722defc09b46d26c5436ed79e30
zhuiks <zhuke@ukr.net>
2020-03-25 16:28:29 +0200

    ChordEditor props.onChange

--- packages/common/src/components/chords-editor/index.tsx
+++ packages/common/src/components/chords-editor/index.tsx
@@ -2,25 +2,24 @@ import React from "react";
 import { Editor, EditorState, DraftHandleValue } from 'draft-js';
 import chordsBlockRenderer from "./chords-block";
 import { useSlide } from "./use-slide";
-import handleChords, { keyBinding } from "./key-binding";
+import { keyBinding } from "./key-binding";
 import ChordSlide from "../../chords/chord-slide";
 
 
 export interface ChordEditorProps {
     slide: ChordSlide;
+    onChange?: (s: ChordSlide) => void;
 }
-const ChordEditor: React.FC<ChordEditorProps> = ({ slide }) => {
-    const { editorState, dispatch } = useSlide(slide);
-    const onChange = (newState: EditorState) => {
+
+const ChordEditor: React.FC<ChordEditorProps> = ({ slide: initialSlide, onChange }) => {
+    const { editorState, slide, dispatch } = useSlide(initialSlide);
+    const onEditorChange = (newState: EditorState) => {
         dispatch({ type: 'SELECTION_CHANGE', editorState: newState })
+        if(onChange && !Object.is(slide, initialSlide)) {
+            onChange(slide);
+        }
     }
-    const onCharInput = (chars: string, es: EditorState) => {
 
-        dispatch({ type: 'ADD_CHORD', editorState: es, payload: handleChords(chars) });
-
-        const handled: DraftHandleValue = "handled";
-        return handled;
-    }
     const onKeyCommand = (command: string, es: EditorState) => {
 
         console.log(command);
@@ -35,7 +34,7 @@ const ChordEditor: React.FC<ChordEditorProps> = ({ slide }) => {
     return (
         <Editor
             editorState={editorState}
-            onChange={onChange}
+            onChange={onEditorChange}
             blockRendererFn={chordsBlockRenderer}
             // handleBeforeInput={onCharInput}
             handleKeyCommand={onKeyCommand}
