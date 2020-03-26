de3a3b645c8cef304a68a04c1082bd0c61ed60af
zhuiks <zhuke@ukr.net>
2020-03-26 19:29:49 +0200

    start with stepper

--- packages/editor/src/organisms/song-form.tsx
+++ packages/editor/src/organisms/song-form.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
-import StepWizard from 'react-step-wizard';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import Badge from 'react-bootstrap/Badge';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import { Chip, Paper, Stepper, Step, StepLabel, StepContent } from '@material-ui/core';
 import Loading from '../atoms/loading';
-
 import AppError from '../molecules/error';
 import { SlideInput } from '../__generated__/globalTypes';
 import textBreaker from '../lib/text-breaker';
 
-import Step from '../molecules/step';
+
 import Orderer from '../molecules/orderer';
 import { useUpdateSong } from '../molecules/submit';
 import SubmitResult from './submit-result';
@@ -38,6 +35,7 @@ const SaveForm: React.FC<SaveFormProps> = ({ songData }) => {
   const [songSlides, setSlides] = useState<SlideInput[]>([]);
   const [songTitle, setTitle] = useState<string>(songData.title || '');
   const [songLinks, setLinks] = useState<string[]>();
+  const [activeStep, setActiveStep] = React.useState(0);
 
   const onStepChange = ({ previousStep, activeStep }: onStepChangeArgs) => {
     if (previousStep === 1) {
@@ -64,51 +62,59 @@ const SaveForm: React.FC<SaveFormProps> = ({ songData }) => {
   if (mutationResult.loading) return <Loading />;
   return (
     <>
-      <Badge variant="info">{songData.id}</Badge>
+      <Chip size="small" icon={<VpnKeyIcon />} label={songData.id} />
       {mutationResult.error &&
         <AppError err={mutationResult.error} />}
       {mutationResult.data ?
         <SubmitResult data={mutationResult.data.updateSong} />
         :
-        <Row className="justify-content-center">
-          <Col sm={6}>
-            <Form>
-              <StepWizard onStepChange={onStepChange}>
-                <Step title="Song Lyrics" isNewSong={isNewSong}>
+        <Paper component="form">
+          <Stepper activeStep={activeStep} orientation="vertical">
+            <Step>
+              <StepLabel>Song Lyrics</StepLabel>
+              <StepContent>
+                <Form.Control
+                  id="song-text"
+                  as="textarea"
+                  rows="10"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setText(e.target.value) }}
+                  value={songText}
+                  style={{ direction: "rtl" }}
+                />
+              </StepContent>
+            </Step>
+            <Step>
+              <StepLabel>Song Order</StepLabel>
+              <StepContent>
+                <Orderer slides={songSlides} />
+              </StepContent>
+            </Step>
+            <Step>
+              <StepLabel>Song Data</StepLabel>
+              <StepContent>
+                <Form.Group controlId="song-title">
                   <Form.Control
-                    id="song-text"
-                    as="textarea"
-                    rows="10"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setText(e.target.value) }}
-                    value={songText}
+                    placeholder="Song title"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setTitle(e.target.value) }}
+                    value={songTitle}
                     style={{ direction: "rtl" }}
                   />
-                </Step>
-                <Step title="Song Order" isNewSong={isNewSong}>
-                  <Orderer slides={songSlides} />
-                </Step>
-                <Step title="Song Data" isNewSong={isNewSong}>
-                  <Form.Group controlId="song-title">
-                    <Form.Control
-                      placeholder="Song title"
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setTitle(e.target.value) }}
-                      value={songTitle}
-                      style={{ direction: "rtl" }}
-                    />
-                  </Form.Group>
-                </Step>
-                <Step title="Extra Details" submit={submitForm} isNewSong={isNewSong}>
-                  <Form.Group controlId="song-link">
-                    <Form.Label>Video Link:</Form.Label>
-                    <Form.Control
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setLinks([e.target.value]) }}
-                    />
-                  </Form.Group>
-                </Step>
-              </StepWizard>
-            </Form>
-          </Col>
-        </Row>
+                </Form.Group>
+              </StepContent>
+            </Step>
+            <Step>
+              <StepLabel>Extra Details</StepLabel>
+              <StepContent>
+                <Form.Group controlId="song-link">
+                  <Form.Label>Video Link:</Form.Label>
+                  <Form.Control
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setLinks([e.target.value]) }}
+                  />
+                </Form.Group>
+              </StepContent>
+            </Step>
+          </Stepper>
+        </Paper>
       }
     </>
   );
