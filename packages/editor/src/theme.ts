da8fa753ef2a13cc13663509fa7583a435ecccd3
zhuiks <zhuke@ukr.net>
2020-03-27 18:00:38 +0300

    colors and fonts

--- packages/editor/src/theme.ts
+++ packages/editor/src/theme.ts
@@ -1,16 +1,28 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-
+const headerFont = {
+  fontFamily: "El Messiri, serif"
+}
 // A custom theme for this app
 const theme = createMuiTheme({
     palette: {
       primary: {
-        main: '#556cd6',
+        main: '#006c65',
+        light: '#459b93',
+        dark: '#00403b',
       },
       secondary: {
-        main: '#008500',
+        main: '#6c0007',
+        light: '#a1382f',
+        dark: '#3f0000',
       },
     },
+    typography: {
+      fontFamily: "Harmattan, sans-serif",
+      h1: headerFont,
+      h2: headerFont,
+      h3: headerFont,
+    }
   });
 
 export default theme;
