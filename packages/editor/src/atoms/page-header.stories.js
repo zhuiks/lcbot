e30bd0756918b57a2671e1484f3ed2a52ec49608
zhuiks <zhuke@ukr.net>
2020-03-30 17:52:09 +0300

    editable pageheader

--- packages/editor/src/atoms/page-header.stories.js
+++ packages/editor/src/atoms/page-header.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import StoryRouter from 'storybook-react-router';
 
@@ -10,6 +10,20 @@ export default {
   decorators: [StoryRouter()],
 };
 
-export const Default = () => <PageHeader>This is Page Title</PageHeader>;
+export const Default = () => <PageHeader>This is Page Title</PageHeader>
 
-export const WithLink = () => <PageHeader link="/test">This is Linked Page Title</PageHeader>;
+export const WithLink = () => <PageHeader link="/test">This is Linked Page Title</PageHeader>
+
+export const Editable = () => {
+  const [text, setText] = useState('Edit Me!');
+  return (
+    <PageHeader
+      onChange={(newText) => {
+        setText(newText);
+      }}
+      helperText="Inline edit is cool!"
+    >
+      {text}
+    </PageHeader>
+  )
+}
--- packages/editor/src/atoms/page-header.tsx
+++ packages/editor/src/atoms/page-header.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import { Link, Typography } from '@material-ui/core';
+import React, { useState }from 'react';
+import { Link, Typography, TextField } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme: Theme) => 
   createStyles({
     root: {
       padding: theme.spacing(2,3,0,3),
+    },
+    // editable: {
+    //   borderBottom: "1px dashed",
+    // },
+    input: {
+      fontSize: theme.typography.h3.fontSize,
+      fontFamily: theme.typography.h3.fontFamily,
+      paddingTop: 0,
     },
   }));
 
 
 interface PageHeaderProps {
   link?: string;
+  onChange?: (s: string) => void;
+  helperText?: string;
   children: any;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ link = '', children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ link = '', onChange, helperText, children }) => {
   const classes = useStyles();
+  const [originalText, setOriginalText] = useState('');
+  const isEditing = originalText!=='';
+  const text = typeof children === 'string' ? children : children.toStirng();
+  const editable = typeof onChange === 'function';
+  const onClick = () => {
+    if(editable && !isEditing) {
+      setOriginalText(text);
+    }
+  }
+  const onKeyPress = (e: React.KeyboardEvent) => {
+    console.log(e.key);
+    if(e.key === 'Enter') {
+      setOriginalText('');
+    }
+    // if(e.key === 'Esc') {
+    //   onChange && onChange(originalText);
+    //   setOriginalText('');
+    // }
+  }
+  if(editable && isEditing) {
+    return (
+      <TextField 
+        autoFocus
+        helperText={helperText}
+        className={classes.root}
+        InputProps={{
+          classes: {
+            input: classes.input,
+          },
+        }}
+        value={text}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => { onChange && onChange(e.target.value) }}
+        onBlur={()=>setOriginalText('')}
+        onKeyPress={onKeyPress}
+      />
+    )
+  }
   return (
-    <Typography className={classes.root} variant="h3" component="h1">
+    <Typography className={classes.root} variant="h3" component="h1" onClick={onClick}>
       {link !== '' ? (
         <Link component={RouterLink} to={link}>{children}</Link>
       ) : (
