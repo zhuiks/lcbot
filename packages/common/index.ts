c120ae85b663e5f59dd1783d1d8a0f64ba06461d
zhuiks <zhuke@ukr.net>
2020-03-31 16:04:43 +0300

    export from common

---
+++ packages/common/index.ts
@@ -0,0 +1,3 @@
+export { default as Chord } from './src/chords/chord'
+export { default as ChordSlide } from './src/chords/chord-slide'
+export { default as ChordsEditor } from './src/components/chords-editor'
--- packages/common/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Chord } from './chords/chord'
-export { default as ChordSlide } from './chords/chord-slide'
-export { default as ChordsEditor } from './components/chords-editor'
