4d7f51c996e773630ec6a64cbed3140d0f1917c8
zhuiks <zhuke@ukr.net>
2020-04-01 16:16:21 +0300

    refactor common package

--- packages/common/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Chord } from './src/chords/chord'
-export { default as ChordSlide } from './src/chords/chord-slide'
-export { default as ChordsEditor } from './src/components/chords-editor'
---
+++ packages/common/src/index.ts
@@ -0,0 +1,4 @@
+export { default as Chord } from './chords/chord'
+export { default as ChordSlide } from './chords/chord-slide'
+export { default as ChordsEditor } from './components/chords-editor'
+export { default as SongSlide } from './components/song-slide'
