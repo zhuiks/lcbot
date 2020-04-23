80d93c0f429c4e6dc435ee81741146553cc8e098
zhuiks <zhuke@ukr.net>
2020-04-23 14:46:50 +0300

    refactor export getChordIndex

--- packages/common/src/chords/index.ts
+++ packages/common/src/chords/index.ts
@@ -4,6 +4,7 @@ import Chord from "./chord";
 export { default as Chord } from './chord';
 export { default as ChordSlide } from './chord-slide';
 export { default as chordAction } from './chord-action';
+export { default as getChordIndex } from './get-chord-index';
 
 export interface IChordSlide {
   type?: SlideType;
