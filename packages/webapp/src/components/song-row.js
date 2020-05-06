66084923bf09df9560c003c30cbe54f0cbaee076
zhuiks <zhuke@ukr.net>
2020-05-06 14:56:29 +0300

    refactor link decoration

--- packages/webapp/src/components/song-row.js
+++ packages/webapp/src/components/song-row.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from "gatsby"
 import styled from "styled-components"
 
-const SongLink = styled.a`
+const SongLink = styled(Link)`
   text-decoration: none;
+  color: #333;
 `
 
 const SongRow = ({ song }) => {
   const { id, title } = song;
   return (
     <li>
-      <Link to={`/${id}`}>
-        <SongLink>{title}</SongLink>
-      </Link>
+      <SongLink to={`/${id}`}>
+        {title}
+      </SongLink>
     </li>
   )
 }
